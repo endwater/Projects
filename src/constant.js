fb341c022e8f3b06c886bc3831202ae430e7b6fa
endwater <43136657+endwater@users.noreply.github.com>
2020-08-27 19:08:09 -0700

    Update constant.js

--- src/constant.js
+++ src/constant.js
@@ -1,6 +1,6 @@
 export const SAT_BASE_URL = "https://www.n2yo.com/rest/v1/satellite";
 
-export const SAT_API_KEY = "DSH8W8-JH22YB-VM796M-4IZB"; // Don’t copy this, use the API license key you get from N2YO
+export const SAT_API_KEY = "API-KEY"; // use the API license key you get from N2YO
 
 export const STARLINK_CATEGORY = "52";
 
